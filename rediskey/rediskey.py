1ce0bbdb2a00e4dafe83b12284b10d1e834f6515
jakeogh <github.com@v6y.net>
2021-02-24 22:06:39 -0700

    auto-commit

--- rediskey/rediskey.py
+++ rediskey/rediskey.py
@@ -20,6 +20,8 @@
 
 #import os
 import sys
+from collections import defaultdict
+from typing import DefaultDict
 
 import click
 import redis
@@ -28,7 +30,8 @@ from enumerate_input import enumerate_input
 from pkg_resources import iter_entry_points
 from retry_on_exception import retry_on_exception
 
-from rediskey import RedisKey, RedisKeyTypeNotFoundError
+from rediskey import RedisKey
+from rediskey import RedisKeyTypeNotFoundError
 
 #from collections import defaultdict
 #from prettyprinter import cpprint, install_extras
@@ -52,7 +55,7 @@ except ImportError:
     ic = eprint
 
 
-def get_size_of_key(r, key):
+def get_length_of_key(r, key):
     key_type = r.type(key)
     if key_type == b'zset':
         return r.zcard(key)
@@ -71,16 +74,53 @@ def get_key_memory_used(r, *, key):
     M_bytes_used = '{}MB'.format(round(bytes_used / 1024 / 1024, 2))
     return bytes_used, k_bytes_used, M_bytes_used
 
-def list_keys_and_sizes(r):
+
+def keys_and_sizes(r):
     keys = r.keys()
     for key in keys:
         key_type = r.type(key)
-        length = get_size_of_key(r=r, key=key)
+        length = get_length_of_key(r=r, key=key)
         result = key.decode('utf8'), key_type.decode('utf8'), length, *get_key_memory_used(r, key=key)
         yield result
 
 
-#@click.command()
+def namespaces_and_sizes(r):
+    namespaces = set()
+    namespace_count: DefaultDict[str, int] = defaultdict(int)
+    namespace_size: DefaultDict[str, int] = defaultdict(int)
+    namespace_values: DefaultDict[str, int] = defaultdict(int)
+    namespace_types: DefaultDict[str, set] = defaultdict(set)
+    broken_namespaces = set()
+    for key, key_type, length, key_memory_used in keys_and_sizes(r):
+        if '#' in key:
+            namespace = key.split('#')[0]
+            namespaces.add(namespace)
+            namespace_count[namespace] += 1
+            #try:
+            namespace_size[namespace] += key_memory_used
+            #except TypeError:
+            #    pass
+            namespace_values[namespace] += length
+            namespace_types[namespace].add(key_type)
+        else:
+            broken_namespaces.add(key)
+
+    ns_list = list(namespaces)
+    ns_list.sort()
+    for namespace in ns_list:
+        type_list = [t for t in namespace_types[namespace]]
+        print(namespace,
+              namespace_count[namespace],
+              namespace_values[namespace],
+              str(int(namespace_size[namespace] / 1024 / 1024)) + 'MB',
+              type_list)
+
+    if broken_namespaces:
+        print("\n\nlen(broken_namesapces):", len(broken_namespaces), file=sys.stderr)
+        for ns in broken_namespaces:
+            print(ns, file=sys.stderr)
+
+
 @click.option('--verbose', is_flag=True)
 @click.option('--debug', is_flag=True)
 @click.option("--printn", is_flag=True)
@@ -121,7 +161,7 @@ def cli(ctx,
 def list_keys(ctx):
     r = redis.Redis(host='127.0.0.1')
 
-    iterator = list_keys_and_sizes(r=r)
+    iterator = keys_and_sizes(r=r)
 
     for index, value in enumerate_input(iterator=iterator,
                                         null=ctx.obj['null'],
@@ -136,6 +176,28 @@ def list_keys(ctx):
             ic(index, value)
 
         print(value, end=ctx.obj['end'])
+
+
+@cli.command()
+@click.pass_context
+def list_namespaces(ctx):
+    r = redis.Redis(host='127.0.0.1')
+
+    namespaces_and_sizes(r=r)
+
+    #for index, value in enumerate_input(iterator=iterator,
+    #                                    null=ctx.obj['null'],
+    #                                    progress=ctx.obj['progress'],
+    #                                    skip=False,
+    #                                    head=False,
+    #                                    tail=False,
+    #                                    debug=ctx.obj['debug'],
+    #                                    verbose=ctx.obj['verbose'],):
+
+    #    if ctx.obj['verbose']:
+    #        ic(index, value)
+
+    #    print(value, end=ctx.obj['end'])
 
 
 @cli.command()
